32e9909b83681722df801b966469565a71a124fd
agrosoftmanofti <63746888+agrosoftmanofti@users.noreply.github.com>
2025-05-21 10:49:47 -0500

    Integrate OpenIddict and Autofac

---
+++ src/Atlas.AbpModules/AtlasAbpModule.cs
@@ -0,0 +1,13 @@
+using Autofac;
+
+namespace Atlas.AbpModules
+{
+    // Placeholder module to register ABP services via Autofac.
+    public class AtlasAbpModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            // TODO: register ABP services here
+        }
+    }
+}
